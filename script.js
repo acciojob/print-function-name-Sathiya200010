ec692f87f1f886fb8a7436b832debca96f6bb737
Sathiya R <rskeerthi01@gmail.com>
2026-07-13 14:51:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-//your JS code here. If required.
+//your JS code here. If require
+document.addEventListener("DOMContentLoaded", function () {
+    document.body.innerText = arguments.callee.name;
+});
