4011178a64793671ed6f3e01c533a49d6b6c9524
Sathiya R <rskeerthi01@gmail.com>
2026-07-13 14:56:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 //your JS code here. If require
-document.addEventListener("DOMContentLoaded", functionName() {
-    document.body.innerText = alert(arguments.callee.name);
-});
+function functionName() {
+    alert(arguments.callee.name);
+}
